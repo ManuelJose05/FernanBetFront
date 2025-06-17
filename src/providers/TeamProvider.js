674c11122aa97a5e56bba31e2848422a5c7e6467
ManuelJose05 <150932456+ManuelJose05@users.noreply.github.com>
2025-06-17 09:00:40 +0200

    Update TeamProvider.js

--- src/providers/TeamProvider.js
+++ src/providers/TeamProvider.js
@@ -4,18 +4,18 @@ export class TeamProvider {
     base_path = 'https://fernanbetbackend.onrender.com/api/v1/'
 
     getAllEquipos = async () => {
-        return await axios.get(`${this.basePath}teams/getAllTeams/`)
+        return await axios.get(`${this.base_path}teams/getAllTeams/`)
     }
 
     getTeamById = async (teamId) => {
-        return await axios.get(`${this.basePath}teams/getTeamById/${teamId}/`)
+        return await axios.get(`${this.base_path}teams/getTeamById/${teamId}/`)
     }
 
     updateTeamById = async (team) => {
-        return await axios.put(`${this.basePath}teams/updateTeam/${team.id}/`, team)
+        return await axios.put(`${this.base_path}teams/updateTeam/${team.id}/`, team)
     }
 
     createTeam = async (team) => {
-        return await axios.post(`${this.basePath}teams/createTeam/`,team)
+        return await axios.post(`${this.base_path}teams/createTeam/`,team)
     }
 }
